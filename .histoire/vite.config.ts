fc49a270c5cb87924d9222e42db4c1910a91ff8a
dhia-rizqullah <dhia.rizqullah@incentro.com>
2024-08-01 16:47:52 +0800

    chore: setup root modules and histoire modules

---
+++ .histoire/vite.config.ts
@@ -0,0 +1,52 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { HstVue } from '@histoire/plugin-vue';
+import alias from '@rollup/plugin-alias';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'node:path';
+import UnoCSS from 'unocss/vite';
+import { defineConfig } from 'vite';
+
+const projectRootDir = resolve(__dirname);
+export default defineConfig({
+  histoire: {
+    outDir: './dist',
+    plugins: [HstVue()],
+    setupFile: './setup.ts',
+    storyMatch: [resolve(projectRootDir, '../packages/akar/src/**/*.story.vue')],
+    theme: {
+      title: 'Akar',
+    },
+    tree: {
+      groups: [
+        { include: (_file) => true, title: 'Components' },
+        { id: 'utilities', title: 'Utilities' },
+      ],
+    },
+    viteNodeInlineDeps: [/@tanstack/],
+  },
+  plugins: [
+    UnoCSS(),
+    vue(),
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: resolve(projectRootDir, '../packages/akar/src'),
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@iconify/vue': './node_modules/@iconify/vue/dist/iconify.mjs',
+    },
+  },
+  server: {
+    fs: {
+      allow: ['..'],
+    },
+    host: true,
+  },
+});
