59ea8053adb6e57551cd1b6051f4530711c3207e
praburangki <praburangki@gmail.com>
2024-08-05 11:57:41 +0800

    chore(APrimitive): create dummy story file

---
+++ packages/akar/src/APrimitive/story/APrimitive.story.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <Story>
+    <Variant title="Setup histoire">
+      <div>
+        Testing APrimitive
+      </div>
+    </Variant>
+  </Story>
+</template>
