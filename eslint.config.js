aa4731e94bb1ce3f41017d1857935d7e5c5562f5
dhiamonkey <dhiamuhammad@gmail.com>
2024-08-05 14:23:58 +0800

    chore: remove yet another unused codes

--- eslint.config.js
+++ eslint.config.js
@@ -10,27 +10,6 @@ export default vinicuncaESLint(
     vue: true,
   },
   {
-    files: ['**/*.vue'],
-    rules: {
-      'import/first': 'off',
-      'import/order': 'off',
-      'max-statements-per-line': ['error', { max: 2 }],
-      'no-console': 'warn',
-      'regexp/no-super-linear-backtracking': 'off',
-      'symbol-description': 'off',
-      'ts/ban-ts-comment': 'warn',
-      'ts/consistent-type-definitions': 'off',
-      'ts/no-empty-object-type': 'off',
-      'ts/no-non-null-asserted-optional-chain': 'off',
-      'ts/no-unsafe-function-type': 'off',
-      'ts/no-unused-expressions': 'off',
-      'unicorn/prefer-dom-node-text-content': 'off',
-      'unicorn/prefer-number-properties': 'off',
-      'unused-imports/no-unused-vars': 'off',
-      'vue/one-component-per-file': 'off',
-    },
-  },
-  {
     files: ['**/*.ts'],
     rules: {
       'sonar/no-duplicate-string': 'off',
