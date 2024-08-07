9529aae6e52abe208e4d919cfb0ebef2f11e129b
dhiamonkey <dhiamuhammad@gmail.com>
2024-08-07 11:52:25 +0800

    chore: cleanup even more unused and unexplainable code

--- packages/akar/vite.config.ts
+++ packages/akar/vite.config.ts
@@ -1,44 +1,10 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 const projectRootDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: {
-        date: resolve(__dirname, 'src/date/index.ts'),
-        index: resolve(__dirname, 'src/index.ts'),
-      },
-      fileName: (format, name) => {
-        return `${name}.${format === 'es' ? 'js' : 'umd.cjs'}`;
-      },
-      name: 'akar',
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        assetFileNames: (chunkInfo) => {
-          if (chunkInfo.name === 'style.css') {
-            return 'index.css';
-          }
-          return chunkInfo.name as string;
-        },
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
-  plugins: [
-    dts({
-      exclude: ['src/**/story/**', 'src/**/*.story.vue'],
-      tsconfigPath: 'tsconfig.build.json',
-    }),
-  ],
   resolve: {
     alias: {
       '~~': resolve(projectRootDir, 'src'),
