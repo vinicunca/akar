9276a325a48fe0d8231af4eaad823e5df8da2866
dhiamonkey <dhiamuhammad@gmail.com>
2024-08-13 17:24:07 +0800

    feat: create disptach handler and emits as props util function

--- packages/akar/src/shared/index.ts
+++ packages/akar/src/shared/index.ts
@@ -1,5 +1,7 @@
 export { createContext } from './create-context';
+export { handleAndDispatchCustomEvent } from './handle-and-dispatch-custom-event';
 export { renderSlotFragments } from './render-slot-fragments';
+export { useEmitAsProps } from './use-emit-as-props';
 export { useFocusGuards } from './use-focus-guards';
 export { useForwardExpose } from './use-forward-expose';
 export { useSize } from './use-size';
