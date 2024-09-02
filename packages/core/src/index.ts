22f213b94542e8c026234f039d3d159ad8825cd0
praburangki <praburangki@gmail.com>
2024-09-02 11:58:55 +0800

    chore: setup build config

---
+++ packages/core/src/index.ts
@@ -0,0 +1,3 @@
+export {
+  useStateMachine,
+} from './shared';
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -9,5 +9,38 @@ export default defineConfig({
     alias: {
       '~~': resolve(projectRootDir, 'src'),
     },
+    // https://vitejs.dev/config/shared-options.html#resolve-dedupe
+    dedupe: [
+      'vue',
+      '@vue/runtime-core',
+    ],
+  },
+
+  build: {
+    lib: {
+      name: 'akar',
+      fileName: (format, name) => {
+        return `${name}.${format === 'es' ? 'js' : 'umd.cjs'}`;
+      },
+      entry: {
+        index: resolve(__dirname, 'src/index.ts'),
+      },
+    },
+    rollupOptions: {
+      /**
+       * We need to make sure not to include external deps that won't be bundeled in the final build
+       */
+      external: [
+        'vue',
+      ],
+      output: {
+        /**
+         * Provide global variables to use in the UMD build
+         */
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
 });
