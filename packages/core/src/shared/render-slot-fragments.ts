a63177eb60eac42f44e00a5813e17c35b0d0586e
praburangki <praburangki@gmail.com>
2024-09-04 07:24:45 +0800

    chore: add docs for state machine

--- packages/core/src/shared/render-slot-fragments.ts
+++ packages/core/src/shared/render-slot-fragments.ts
@@ -4,6 +4,7 @@ export function renderSlotFragments(children?: Array<VNode>): Array<VNode> {
   if (!children) {
     return [];
   }
+
   return children.flatMap((child) => {
     if (child.type === Fragment) {
       return renderSlotFragments(child.children as Array<VNode>);
