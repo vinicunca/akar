2be0d29ac5bfc78297afb32efd269a6f72d045f3
praburangki <praburangki@gmail.com>
2024-09-11 17:29:21 +0800

    feat: add config provider component

---
+++ packages/core/src/a-config-provider/_a-config-provider.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// import DropdownMenu from '../DropdownMenu/story/_DropdownMenu.vue';
+import AConfigProvider, { type AConfigProviderProps } from './a-config-provider.vue';
+
+const props = withDefaults(
+  defineProps<AConfigProviderProps>(),
+  {
+    scrollBody: undefined,
+  },
+);
+</script>
+
+<template>
+  <AConfigProvider v-bind="props">
+    <!-- <DropdownMenu :default-open="true" /> -->
+  </AConfigProvider>
+</template>
---
+++ packages/core/src/a-config-provider/a-config-provider.vue
@@ -0,0 +1,67 @@
+<script lang="ts">
+import type { Direction, ScrollBodyOption } from '~~/shared/types';
+
+import { createContext } from '~~/shared';
+
+interface AConfigProviderContextValue {
+  dir?: Ref<Direction>;
+  locale?: Ref<string>;
+  nonce?: Ref<string | undefined>;
+  scrollBody?: Ref<boolean | ScrollBodyOption>;
+  useId?: () => string | undefined;
+}
+
+export const [
+  injectConfigProviderContext,
+  provideConfigProviderContext,
+] = createContext<AConfigProviderContextValue>('AConfigProvider');
+
+export interface AConfigProviderProps {
+  /**
+   * The global reading direction of your application. This will be inherited by all primitives.
+   * @defaultValue 'ltr'
+   */
+  dir?: Direction;
+  /**
+   * The global locale of your application. This will be inherited by all primitives.
+   * @defaultValue 'en'
+   */
+  locale?: string;
+  /**
+   * The global `nonce` value of your application. This will be inherited by the related primitives.
+   */
+  nonce?: string;
+  /**
+   * The global scroll body behavior of your application. This will be inherited by the related primitives.
+   */
+  scrollBody?: boolean | ScrollBodyOption;
+}
+</script>
+
+<script lang="ts" setup>
+import { type Ref, toRefs, useId } from 'vue';
+
+const props = withDefaults(
+  defineProps<AConfigProviderProps>(),
+  {
+    dir: 'ltr',
+    locale: 'en',
+    scrollBody: true,
+    nonce: undefined,
+  },
+);
+
+const { dir, locale, scrollBody, nonce } = toRefs(props);
+
+provideConfigProviderContext({
+  dir,
+  locale,
+  scrollBody,
+  nonce,
+  useId,
+});
+</script>
+
+<template>
+  <slot />
+</template>
