e6e9613b46fbaffbd3d669eee0deccfcd5019d3d
praburangki <praburangki@gmail.com>
2024-10-03 09:26:21 +0800

    feat: add usePrimitiveElement

--- packages/core/src/a-primitive/index.ts
+++ packages/core/src/a-primitive/index.ts
@@ -1,2 +1,3 @@
 export { APrimitive, type APrimitiveProps, type AsTag } from './a-primitive';
 export { APrimitiveSlot } from './a-slot';
+export { usePrimitiveElement } from './use-primitive-element';
