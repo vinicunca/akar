da10a620f690f8a52d7bb28ab7beef8ad44a6b0d
praburangki <praburangki@gmail.com>
2024-10-07 21:35:36 +0800

    feat: add use form control composable

--- packages/core/src/shared/index.ts
+++ packages/core/src/shared/index.ts
@@ -7,6 +7,7 @@ export { useBodyScrollLock } from './use-body-scroll-lock';
 export { useDirection } from './use-direction';
 export { useEmitAsProps } from './use-emit-as-props';
 export { useFocusGuards } from './use-focus-guards';
+export { useFormControl } from './use-form-control';
 export { useForwardExpose } from './use-forward-expose';
 export { useForwardProps } from './use-forward-props';
 export { useForwardPropsEmits } from './use-forward-props-emits';
--- packages/core/src/shared/types.ts
+++ packages/core/src/shared/types.ts
@@ -1,6 +1,14 @@
 export type Direction = 'ltr' | 'rtl';
+export type DataOrientation = 'horizontal' | 'vertical';
 
 export interface ScrollBodyOption {
   margin?: boolean | number;
   padding?: boolean | number;
+}
+
+export interface FormFieldProps {
+  /** The name of the field. Submitted with its owning form as part of a name/value pair. */
+  name?: string;
+  /** When `true`, indicates that the user must set the value before the owning form can be submitted. */
+  required?: boolean;
 }
