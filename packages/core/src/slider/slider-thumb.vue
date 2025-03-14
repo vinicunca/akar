58ece6d9f26c3e111c18caef907af2b126beb7fe
praburangki <praburangki@gmail.com>
2025-03-15 06:56:17 +0800

    fix(slider): thumb position incorrectly

--- packages/core/src/slider/slider-thumb.vue
+++ packages/core/src/slider/slider-thumb.vue
@@ -15,7 +15,11 @@ const { getItems } = useCollection();
 
 const { forwardRef, currentElement: thumbElement } = useForwardExpose();
 
-const index = computed(() => thumbElement.value ? getItems().findIndex((i) => i.ref === thumbElement.value) : -1);
+const index = computed(() =>
+  thumbElement.value
+    ? getItems(true).findIndex((i) => i.ref === thumbElement.value)
+    : -1,
+);
 </script>
 
 <template>
