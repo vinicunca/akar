3d5ab8b004cca2f55db9582e4083b4260cda8e83
praburangki <praburangki@gmail.com>
2025-09-26 10:45:24 +0800

    chore: add content typings

---
+++ packages/pohon/src/runtime/types/content.ts
@@ -0,0 +1,4 @@
+export * from '../components/content/content-navigation.vue';
+export * from '../components/content/content-search.vue';
+export * from '../components/content/content-surround.vue';
+export * from '../components/content/content-toc.vue';
