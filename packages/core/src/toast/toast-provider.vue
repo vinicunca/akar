7d6fac0717a3401ee36390c5597088752b846f69
praburangki <praburangki@gmail.com>
2025-10-06 07:40:45 +0800

    feat(Toast): support disabling swipe-to-close with the disableSwipe prop

--- packages/core/src/toast/toast-provider.vue
+++ packages/core/src/toast/toast-provider.vue
@@ -7,6 +7,7 @@ import { createContext } from '~~/shared';
 type ToastProviderContext = {
   label: Ref<string>;
   duration: Ref<number>;
+  disableSwipe: Ref<boolean>;
   swipeDirection: Ref<SwipeDirection>;
   swipeThreshold: Ref<number>;
   toastCount: Ref<number>;
@@ -31,6 +32,11 @@ export interface AToastProviderProps {
    */
   duration?: number;
   /**
+   * Whether to disable the ability to swipe to close the toast.
+   * @defaultValue false
+   */
+  disableSwipe?: boolean;
+  /**
    * Direction of pointer swipe that should close the toast.
    * @defaultValue 'right'
    */
@@ -42,8 +48,10 @@ export interface AToastProviderProps {
   swipeThreshold?: number;
 }
 
-export const [injectAToastProviderContext, provideToastProviderContext]
-  = createContext<ToastProviderContext>('AToastProvider');
+export const [
+  injectAToastProviderContext,
+  provideToastProviderContext,
+] = createContext<ToastProviderContext>('AToastProvider');
 </script>
 
 <script setup lang="ts">
@@ -53,13 +61,16 @@ defineOptions({
   inheritAttrs: false,
 });
 
-const props = withDefaults(defineProps<AToastProviderProps>(), {
-  label: 'Notification',
-  duration: 5000,
-  swipeDirection: 'right',
-  swipeThreshold: 50,
-});
-const { label, duration, swipeDirection, swipeThreshold } = toRefs(props);
+const props = withDefaults(
+  defineProps<AToastProviderProps>(),
+  {
+    label: 'Notification',
+    duration: 5000,
+    swipeDirection: 'right',
+    swipeThreshold: 50,
+  },
+);
+const { label, duration, disableSwipe, swipeDirection, swipeThreshold } = toRefs(props);
 useCollection({ isProvider: true });
 
 const viewport = ref<HTMLElement>();
@@ -75,6 +86,7 @@ if (props.label && typeof props.label === 'string' && !props.label.trim()) {
 provideToastProviderContext({
   label,
   duration,
+  disableSwipe,
   swipeDirection,
   swipeThreshold,
   toastCount,
