bd6f473fce3191b2fc4cef21bad9c6828078c7d9
praburangki <praburangki@gmail.com>
2026-02-13 13:47:36 +0800

    chore: remove dashboard menu layer

--- packages/pohon/playgrounds/admin/layers/dashboard-menu/lib/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as DashboardMenuBadge } from './dashboard-menu-badge.vue';
-export { default as DashboardMenuMenuItem } from './dashboard-menu-menu-item.vue';
-export {
-  default as DashboardMenuNormalMenu,
-  type DashboardMenuNormalMenuProps,
-} from './dashboard-menu-normal-menu.vue';
-export { default as DashboardMenuRoot } from './dashboard-menu-root.vue';
-export { default as DashboardMenuSubMenuRoot } from './dashboard-menu-sub-menu-root.vue';
--- packages/pohon/playgrounds/admin/layers/dashboard-menu/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  DashboardMenuNormalMenu,
-  type DashboardMenuNormalMenuProps,
-} from './components';
-export * from './dashboard-menu.types';
-export { default as DashboardMenu } from './dashboard-menu.vue';
