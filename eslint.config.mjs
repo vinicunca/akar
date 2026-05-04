8c2a74424df95be75e76ef6983c1daa2da4c8629
praburangki <praburangki@gmail.com>
2026-05-04 11:20:12 +0800

    chore: init repo

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import { vinicuncaESLint } from '@vinicunca/eslint-config';
+
+export default vinicuncaESLint(
+  {
+    vue: {
+      overrides: {
+        'vue/max-attributes-per-line': ['error', {
+          singleline: 1,
+          multiline: 1,
+        }],
+      },
+    },
+    typescript: true,
+    markdown: {
+      overrides: {
+        'vue/max-attributes-per-line': 'off', // in documentation we allow more attributes per line
+      },
+    },
+  },
+  {
+    ignores: ['*.js'],
+  },
+  {
+    rules: {
+      'ts/no-non-null-asserted-optional-chain': 'off',
+      'ts/ban-ts-comment': 'warn',
+      'ts/consistent-type-definitions': 'off',
+      'ts/no-unsafe-function-type': 'off',
+      'ts/no-unused-expressions': 'off',
+      'ts/no-empty-object-type': 'off',
+      'symbol-description': 'off',
+      'no-console': 'warn',
+      'import/first': 'off',
+      'import/order': 'off',
+      'style/max-statements-per-line': ['error', { max: 2 }],
+      'vue/one-component-per-file': 'off',
+      'unicorn/prefer-dom-node-text-content': 'off',
+      'unicorn/prefer-number-properties': 'off',
+      'unused-imports/no-unused-vars': 'off',
+      'regexp/no-super-linear-backtracking': 'off',
+      'markdown/heading-increment': 'off',
+      'markdown/no-multiple-h1': 'off',
+    },
+  },
+  {
+    files: ['*.story.vue'],
+    rules: {
+      'no-console': 'off',
+      'no-alert': 'off',
+      'unused-imports/no-unused-vars': 'off',
+    },
+  },
+  {
+    files: ['**/package.json'],
+    rules: {
+      // Wrecks the order of `files` otherwise, and breaks the exclusion patterns
+      // pnpm has no issues with that, but npm does and doesn't apply the correct config
+      'jsonc/sort-array-values': 'off',
+    },
+  },
+);
